688563f2c15d5f4b415e313ee232d9209d1e5c87
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-08 09:48:35 -0700

    add promise WIP

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './css/styles.css';
-
-function getRates() {
-  let request = new XMLHttpRequest();
-  const url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
-
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response);
-    }
-  });
-
-  request.open("GET", url, true);
-  request.send();
-}
+import { Rates, mult } from './js/apiLogic.js';
 
 
 
 
-//UI logic
+
+
+
+
 
--- src/js/apiLogic.js
+++ src/js/apiLogic.js
@@ -1,37 +1,26 @@
-class Bikes {
-  getBikes() {
-    return new Promise(function (resolve, reject) {
+
+export class Rates{
+  static getRates() {
+    return new Promise(function (resolve, reject){
       let request = new XMLHttpRequest();
-      const urlToSend = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
       request.addEventListener("loadend", function() {
-        const serverResponse = JSON.parse(this.responseText);
-        console.log(serverResponse);
-      });  
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response]);
+        } else {
+          reject([this]);
+        }
+    });
+    request.open("GET", url, true);
+    request.send();
     });
   }
-}  
+
+  export function mult(){
+    let output = ;
+
+  }
+}
 
 
-// export class Bikes {
-//   static getBikes(curr) {
-//     return new Promise(function (resolve, reject) {
-//       let request = new XMLHttpRequest();
-//       const urlToSend = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${curr}`;
-//       request.addEventListener("loadend", function() {
-//         const serverResponse = JSON.parse(this.responseText);
-//         if (this.status === 200) {
-//           resolve([serverResponse]);
-//         } else {
-//           reject([this]);
-//         }
-//       });
-//         request.open("GET", urlToSend, true);
-//         request.send();
-//     });
-//   }
-// }  
-
-// export function exchange(apiResponse) {
-//   const usdTo = ;
-
-// }
