2b4098673e3597b4d3d4283a03d832d734effac9
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-08 15:24:36 -0700

    add country code to output

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './css/styles.css';
 import { Rates, mult } from './js/apiLogic.js';
 
 
-function showRate(userInputNum, userInputCurMult) {
-  let res = mult(userInputNum, userInputCurMult);
+function showRate(userInputNum, userInputCurMult, userInputCur) {
+  let res = mult(userInputNum, userInputCurMult, userInputCur);
   document.getElementById("form1").reset();
   document.getElementById("showResults").innerText = res;
+  document.getElementById("showCountryCode").innerText = userInputCur;
+  console.log(userInputCur);
 }
 function showError(request) {
   document.getElementById("showResults").innerText = `There is an error accessing the bike ${request[0].status}`;
@@ -25,7 +27,7 @@ function handleFormSubmission(event) {
       document.getElementById("showResults").innerText = "invalid currency code, please try again";
       document.getElementById("form1").reset();
     } else {
-      showRate(userInputNum, userInputCurMult);
+      showRate(userInputNum, userInputCurMult, userInputCur);
     }
   }, function (errorMessage) {
     showError(errorMessage);
